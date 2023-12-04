771270570d765349d2485101284619bb7a79d124
Christos Papadopoulos <chr.papadopoulos@kaizengaming.com>
2023-12-04 11:45:36 +0200

    day2 / day3 part 1

---
+++ Day2/Program.cs
@@ -0,0 +1,58 @@
+﻿
+// only 12 red cubes, 13 green cubes, and 14 blue cubes
+
+var lines = File.ReadAllLines("C:\\repos\\AdventOfCode\\2023\\Day2\\input.txt");
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+var sum = 0;
+var power = 0;
+foreach (var line in lines)
+{
+
+    var t = line.Split(':');
+    var gameId = int.Parse(t[0].Where(char.IsDigit).ToArray());
+
+    var games = t[1].Split(';');
+    var gameIsPossible = true;
+
+    var minimumRed = 0;
+    var minimumGreen = 0;
+    var minimumBlue = 0;
+
+    foreach (var game in games)
+    {
+        var round = game.Split(',');
+        var red = int.Parse(round.FirstOrDefault(x => x.Contains("red"))?.Where(char.IsDigit).ToArray() ?? new[] { '0' });
+        var blue = int.Parse(round.FirstOrDefault(x => x.Contains("blue"))?.Where(char.IsDigit).ToArray() ?? new[] { '0' });
+        var green = int.Parse(round.FirstOrDefault(x => x.Contains("green"))?.Where(char.IsDigit).ToArray() ?? new[] { '0' });
+        if (red > 12 || green > 13 || blue > 14)
+        {
+            gameIsPossible = false;
+        }
+
+        //part 2
+        if (red > minimumRed)
+        {
+            minimumRed = red;
+        }
+
+        if (green > minimumGreen)
+        {
+            minimumGreen = green;
+        }
+
+        if (blue > minimumBlue)
+        {
+            minimumBlue = blue;
+        }
+
+    }
+    if (gameIsPossible)
+    {
+        sum += gameId;
+    }
+
+    power += minimumBlue * minimumRed * minimumGreen;
+}
+Console.WriteLine(sum);
+Console.WriteLine(power);
---
+++ Day3/Program.cs
@@ -0,0 +1,106 @@
+﻿
+using System.Linq;
+
+var allLines = File.ReadAllLines("C:\\code\\advent-of-code-2023\\Day3\\input.txt");
+var numberPositions = new List<Num>();
+var specialChars = new List<SpecialChar>();
+var lineCount = 0;
+
+foreach (var line in allLines)
+{
+    var charToPosition = new List<(char, int)>();
+    var i = 0;
+
+    foreach(var cha in line)
+    {
+        if (char.IsDigit(cha))
+        {
+            charToPosition.Add((cha, i));
+        }
+        else if (cha == '.' && charToPosition.Count != 0)
+        {
+            var tt = new Num
+            {
+                Number = new string(charToPosition.Select(x => x.Item1).ToArray()),
+                Positions = charToPosition.Select(x => x.Item2).ToArray(),
+                Line = lineCount
+            };
+            numberPositions.Add(tt);
+            charToPosition.Clear();
+        }
+        else if(!char.IsLetterOrDigit(cha) && cha != '.')
+        {
+            specialChars.Add(new SpecialChar
+            {
+                Line = lineCount,
+                Position = i
+            });
+            
+            var tt = new Num
+            {
+                Number = new string(charToPosition.Select(x => x.Item1).ToArray()),
+                Positions = charToPosition.Select(x => x.Item2).ToArray(),
+                Line = lineCount
+            };
+            numberPositions.Add(tt);
+            charToPosition.Clear();
+        }
+
+        if (charToPosition.Count != 0 && i == line.Length - 1)
+        {
+            var tt = new Num
+            {
+                Number = new string(charToPosition.Select(x => x.Item1).ToArray()),
+                Positions = charToPosition.Select(x => x.Item2).ToArray(),
+                Line = lineCount
+            };
+            numberPositions.Add(tt);
+            charToPosition.Clear();
+        }
+
+        i++;
+    }
+    lineCount++;
+}
+var sum = 0;
+
+foreach( var line in Enumerable.Range(0, allLines.Length))
+{
+    foreach(var lineChar in specialChars.Where(x => x.Line == line))
+    {
+        foreach(var t in numberPositions.Where(x =>
+            x.Line == lineChar.Line ||
+            x.Line == lineChar.Line - 1 ||
+            x.Line == lineChar.Line + 1
+        ).Where(x =>
+            x.Positions.Any(y => lineChar.Position == y || lineChar.Position - 1 == y || lineChar.Position + 1 == y)
+        ).Where(x =>
+            !x.HasBeenUsed))
+        {
+            t.ChangeHasBeenUsed();
+            sum += t.Value;
+        }
+    }
+}
+Console.WriteLine(sum);
+
+
+class Num
+{
+    public string Number { get; set; }
+    public int Value => int.Parse(Number);
+    public int[] Positions { get; set; }
+    public int Line { get; set; }
+    public bool HasBeenUsed { get; set; }
+
+    public void ChangeHasBeenUsed()
+    {
+        HasBeenUsed = true;
+    }
+}
+
+struct SpecialChar
+{
+    public int Position { get; set; }
+    public int Line { get; set; }
+}
