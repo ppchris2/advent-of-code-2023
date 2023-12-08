9cb50cd456830e8b1e5a99114edce07d6dca3c62
Christos Papadopoulos <chr.papadopoulos@kaizengaming.com>
2023-12-08 10:25:26 +0200

    day 7 / day 8 parts 1

---
+++ Day7/Program.cs
@@ -0,0 +1,123 @@
+﻿var example = @"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+KK5JJ 483";
+
+var input = File.ReadAllLines("C:\\code\\advent-of-code-2023\\Day7\\input.txt");
+
+var games = example.Split('\n').Select(x =>
+{
+    var y = x.Split(' ').ToArray();
+    return new Game() {Hand = y[0], Bid = int.Parse(y[1])};
+});
+
+var groups = games.GroupBy(x => x.FindType()).OrderByDescending(x => x.Key);
+var rank = 1;
+var result = 0;
+foreach (var group in groups)
+{
+    var t = group.OrderByDescending(x => x.Hand, new Comp()).ToArray();
+    foreach (var tt in t)
+    {
+        result += rank++ * tt.Bid;
+
+    }
+}
+Console.WriteLine(result);
+
+public class Comp : IComparer<string>
+{
+    public static List<char> Order = new List<char>
+    {
+        'A',
+        'K',
+        'Q',
+        'J',
+        'T',
+        '9',
+        '8',
+        '7',
+        '6',
+        '5',
+        '4',
+        '3',
+        '2'
+    };
+    
+    public int Compare(string? x, string? y)
+    {
+        foreach (var cha in x.Zip(y))
+        {
+            if (Order.IndexOf(cha.First) == Order.IndexOf(cha.Second))
+            {
+                continue; 
+            }
+            if (Order.IndexOf(cha.First) > Order.IndexOf(cha.Second))
+            {
+                return 1;
+                
+            }
+
+            return -1;
+        }
+
+        return 0;
+    }
+}
+
+class Game
+{
+    public string Hand { get; set; }
+    public int Bid { get; set; }
+    
+    
+    
+    // public int FindScore
+    // {
+    //     get
+    //     {
+    //     
+    //         var score = 0;
+    //         foreach (var cha in Hand)
+    //         {
+    //             score += Order.IndexOf(cha);
+    //         }
+    //
+    //         return score;
+    //     }
+    // }
+
+    public Types FindType()
+    {
+        // 32T3K one pair
+        var doublePairs = Hand.GroupBy(c => c).Where(g=>g.Count() == 2).ToArray();
+        var groups = Hand.GroupBy(c => c).Max(g=>g.Count());
+        return groups switch
+        {
+            5 => Types.FiveOfKind,
+            4 => Types.FourOfKind,
+            3 => Types.ThreeOfKind,
+            2  when doublePairs.Count() == 2 => Types.TwoPair,
+            2 when doublePairs.Count() == 1 => Types.OnePair,
+            _ => Types.NoPair
+        };
+    }
+    
+}
+
+enum Types
+{
+    FiveOfKind,
+    FourOfKind,
+    ThreeOfKind,
+    TwoPair,
+    OnePair,
+    NoPair
+}
+enum HandStrengths
+{
+    // A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2
+    
+    
+}
---
+++ Day8/Program.cs
@@ -0,0 +1,97 @@
+﻿// var instructions = "LRLRLLRLRLRRLRLRLRRLRLRLLRRLRRLRLRLRLLRRRLRRRLLRRLRLRLRRRLRRLRRRLRLRLRRLRLLRLRLRRLRRRLRLRRLRRRLLRLRLRRRLRRRLRLRRRLRLRRRLLRRLLLRRRLLRRRLRRRLRRRLRLRLRLLRLRRLRLRLLLRRLRRLRRLRLRRLRRLLRRLRLRRRLRLRLLRRRLRRRLRRRLLLRRRLRLRLRRLRRRLRRRLRLRRRLRRLRRRLRLRRLLRRRLRRRLLLRRLRLRLRRLRRRLRRLRRLRLRRRR";
+//
+// var input = File.ReadAllLines("C:\\code\\advent-of-code-2023\\Day8\\input.txt");
+// var tt = input.Select(x => {
+//
+//     var a = x.Replace("=", "").Replace("(", "").Replace(")", "").Replace(",", "").Split(' ').Where(X => X != "").ToArray();
+//     return new Map(a[0], a[1], a[2]);
+//     
+// }).ToDictionary(x => x.Source, x => x);
+//
+// var current = tt.First(x => x.Key == "AAA").Value;
+// var steps = 0;
+// var foundIt = false;
+// while (true)
+// {
+//     foreach (var nextStep in instructions.Select(dir => dir == 'L' ? current.Left : current.Right))
+//     {
+//         current = tt[nextStep];
+//         steps++;
+//
+//         if (current.Source == "ZZZ")
+//         {
+//             foundIt = true;
+//             break;
+//         }
+//     }
+//
+//     if (foundIt)
+//     {
+//         break;
+//     }
+// }
+// Console.WriteLine(steps);
+//
+//
+// struct Map
+// {
+//     public Map(string source, string left, string right)
+//     {
+//         Source = source;
+//         Left = left;
+//         Right = right;
+//     }
+//     public string Source { get; set; }
+//     public string Left { get; set; }
+//     public string Right { get; set; }
+// }
+
+
+
+//part 2
+var instructions = "LRLRLLRLRLRRLRLRLRRLRLRLLRRLRRLRLRLRLLRRRLRRRLLRRLRLRLRRRLRRLRRRLRLRLRRLRLLRLRLRRLRRRLRLRRLRRRLLRLRLRRRLRRRLRLRRRLRLRRRLLRRLLLRRRLLRRRLRRRLRRRLRLRLRLLRLRRLRLRLLLRRLRRLRRLRLRRLRRLLRRLRLRRRLRLRLLRRRLRRRLRRRLLLRRRLRLRLRRLRRRLRRRLRLRRRLRRLRRRLRLRRLLRRRLRRRLLLRRLRLRLRRLRRRLRRLRRLRLRRRR";
+
+var input = File.ReadAllLines("C:\\code\\advent-of-code-2023\\Day8\\example.txt");
+var indexOfLetters = new Dictionary<char, int>();
+
+var tt = input.Select(x => {
+
+    var a = x.Replace("=", "").Replace("(", "").Replace(")", "").Replace(",", "").Split(' ').Where(X => X != "").ToArray();
+
+    for (var index = 0; index < a.Length; index++)
+    {
+        var letters = a[index];
+        var last = letters.Last();
+        if (indexOfLetters.ContainsKey(last))
+        {
+            var newIndex = ++indexOfLetters[last];
+            letters = $"{newIndex}{newIndex}{letters[0]}";
+        }
+        else
+        {
+            indexOfLetters.Add(last, 1);
+            letters = $"{1}{1}{letters[0]}";
+
+        }
+
+        a[index] = letters;
+    }
+
+    return new Map(a[0], a[1], a[2]);
+    
+}).ToDictionary(x => x.Source, x => x);
+
+;
+
+struct Map
+{
+    public Map(string source, string left, string right)
+    {
+        Source = source;
+        Left = left;
+        Right = right;
+    }
+    public string Source { get; set; }
+    public string Left { get; set; }
+    public string Right { get; set; }
+}
