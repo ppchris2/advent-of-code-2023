a35a5c08b18bbfb20f157ef165635e199b99ca54
Christos <ppchris2@gmail.com>
2023-12-10 11:30:45 +0200

    day 10 wip

---
+++ Day10/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+var example1 = @"-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF";
+
+var example2 = @"7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ";
+
+
+
+//var lines = example2
+//    .Split('\n')
+//    .ToArray();
+
+var lines = File.ReadAllLines("C:\\Users\\ppchr\\source\\repos\\advent-of-code-2023\\Day10\\input.txt");
+
+var line = Array.FindIndex(lines, x => x.Contains("S"));
+var startingPoint = lines[line].ToArray();
+var position = Array.FindIndex(startingPoint, x => x=='S');
+
+int distance = 0;
+TraverseDepthFirst(new Point(line, position), lines, null);
+
+Console.WriteLine(distance);
+
+int TraverseDepthFirst(Point currentPosition, string[] matrix, Point? previousPosition)
+{
+    var currentTile = matrix[currentPosition.X][currentPosition.Y];
+    
+    if (currentTile == 'S' && distance != 0)
+    {
+        return distance;
+    }
+
+    var steps = GetPossibleNextSteps(currentTile, currentPosition)
+        .Where(x => x != previousPosition &&
+        x.X >= 0 &&
+        x.Y >= 0 &&
+        x.X < matrix.Length &&
+        x.Y < matrix[0].Length &&
+        matrix[x.X][x.Y] != '.' );
+    distance++;
+    
+
+    foreach (var step in steps)
+    {
+        return TraverseDepthFirst(step, matrix, currentPosition);
+    }
+    throw new Exception();
+}
+
+Point[] GetPossibleNextSteps(char currentTile, Point currentPosition)
+{
+    Point katw = new Point(currentPosition.X + 1, currentPosition.Y);
+    Point panw = new Point(currentPosition.X - 1, currentPosition.Y);
+    Point deksia = new Point(currentPosition.X, currentPosition.Y + 1);
+    Point aristera = new Point(currentPosition.X, currentPosition.Y - 1);
+    switch (currentTile)
+    {
+        case 'S':
+            return new[] { 
+                panw, katw, deksia, aristera
+            };
+        case '-':
+            return new[] {
+                deksia, aristera
+            };
+        case '|':
+            return new[] {
+                panw, katw
+            };
+        case 'L':
+            return new[] {
+                panw, deksia
+            };
+        case 'J':
+            return new[] {
+                panw, aristera
+            };
+        case '7':
+            return new[] {
+                aristera, katw
+            };
+        case 'F':
+            return new[] {
+                deksia, katw
+            };
+        default:
+            return Array.Empty<Point>();
+    }
+
+}
--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,10 +1,7 @@
-﻿var example = @"32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-KK5JJ 483";
+﻿var example = @"2233J 1
+aabbJ 2";
 
-var input = File.ReadAllLines("C:\\code\\advent-of-code-2023\\Day7\\input.txt");
+//var input = File.ReadAllLines("C:\\Users\\ppchr\\Source\\Repos\\advent-of-code-2023\\Day7\\input.txt");
 
 var games = example.Split('\n').Select(x =>
 {
@@ -12,7 +9,7 @@ var games = example.Split('\n').Select(x =>
     return new Game() {Hand = y[0], Bid = int.Parse(y[1])};
 });
 
-var groups = games.GroupBy(x => x.FindType()).OrderByDescending(x => x.Key);
+var groups = games.GroupBy(x => x.FindTypePart2()).OrderByDescending(x => x.Key);
 var rank = 1;
 var result = 0;
 foreach (var group in groups)
@@ -44,16 +41,33 @@ public class Comp : IComparer<string>
         '3',
         '2'
     };
-    
+
+    public static List<char> OrderPart2 = new List<char>
+    {
+        'A',
+        'K',
+        'Q',
+        'T',
+        '9',
+        '8',
+        '7',
+        '6',
+        '5',
+        '4',
+        '3',
+        '2',
+        'J'
+    };
+
     public int Compare(string? x, string? y)
     {
         foreach (var cha in x.Zip(y))
         {
-            if (Order.IndexOf(cha.First) == Order.IndexOf(cha.Second))
+            if (OrderPart2.IndexOf(cha.First) == OrderPart2.IndexOf(cha.Second))
             {
                 continue; 
             }
-            if (Order.IndexOf(cha.First) > Order.IndexOf(cha.Second))
+            if (OrderPart2.IndexOf(cha.First) > OrderPart2.IndexOf(cha.Second))
             {
                 return 1;
                 
@@ -72,44 +86,60 @@ class Game
     public int Bid { get; set; }
     
     
-    
-    // public int FindScore
-    // {
-    //     get
-    //     {
-    //     
-    //         var score = 0;
-    //         foreach (var cha in Hand)
-    //         {
-    //             score += Order.IndexOf(cha);
-    //         }
-    //
-    //         return score;
-    //     }
-    // }
-
     public Types FindType()
     {
-        // 32T3K one pair
         var doublePairs = Hand.GroupBy(c => c).Where(g=>g.Count() == 2).ToArray();
         var groups = Hand.GroupBy(c => c).Max(g=>g.Count());
         return groups switch
         {
             5 => Types.FiveOfKind,
             4 => Types.FourOfKind,
+            3 when doublePairs.Count() == 1 => Types.FullHouse,
             3 => Types.ThreeOfKind,
             2  when doublePairs.Count() == 2 => Types.TwoPair,
             2 when doublePairs.Count() == 1 => Types.OnePair,
             _ => Types.NoPair
         };
     }
-    
+
+    public Types FindTypePart2()
+    {
+        //T3T3J
+        var jokers = Hand.Where(x => x == 'J').Count();
+        if(jokers == 5)
+        {
+            return Types.FiveOfKind;
+        }
+        var newHand = Hand.Replace("J", "");
+
+        var doublePairs = newHand.GroupBy(c => c ).Where(g => { 
+            if(g.Count() + jokers == 3 && jokers == 1)
+            {
+                return true;
+            }
+            return g.Count() + jokers == 2;
+                //
+        }).ToArray();
+        var groups = newHand.GroupBy(c => c).Max(g => g.Count() + jokers);
+        return groups switch
+        {
+            5 => Types.FiveOfKind,
+            4 => Types.FourOfKind,
+            3 when doublePairs.Count() != 0 => Types.FullHouse,
+            3 => Types.ThreeOfKind,
+            2 when doublePairs.Count() == 2 => Types.TwoPair,
+            2 => Types.OnePair,
+            _ => Types.NoPair
+        };
+    }
+
 }
 
 enum Types
 {
     FiveOfKind,
     FourOfKind,
+    FullHouse,
     ThreeOfKind,
     TwoPair,
     OnePair,
